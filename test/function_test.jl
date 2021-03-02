8fe1c420976cc7ea197db4e2fce2e0cacbb5d1cc
Sungho Shin <sungho.shin.ss@gmail.com>
2021-03-01 23:29:15 -0600

    test added and token added

--- test/function_test.jl
+++ test/function_test.jl
@@ -1,23 +1,30 @@
-for (f,points) in [
-    (x->x[1], [randn(1) for i=1:3]),
-    (x->x[1]+1., [randn(1) for i=1:3]),
-    (x->2x[1], [randn(1) for i=1:3]),
-    (x->-x[1], [randn(1) for i=1:3]),
-    (x->x[1]^2, [randn(1) for i=1:3]),
-    (x->x[1]^2.5, [rand(1) for i=1:3]),
-    (x->1/x[1], [randn(1) for i=1:3]),
-    (x->1/x[1], [randn(1) for i=1:3]),
-    (x->sin(x[1]), [randn(1) for i=1:3]),
-    (x->cos(x[1]), [randn(1) for i=1:3]),
-    (x->exp(x[1]), [randn(1) for i=1:3]),
-    (x->x[1]+x[2], [randn(2) for i=1:3]),
-    (x->x[1]-x[2], [randn(2) for i=1:3]),
-    (x->x[1]*x[2], [randn(2) for i=1:3]),
-    (x->x[1]/x[2], [randn(2) for i=1:3]),
-    (x->sin(x[1]/x[2]), [randn(2) for i=1:3]),
-    (x->cos(x[1]-x[2])^2, [randn(2) for i=1:3]),
-    (x->exp(2^x[1]+x[2]^3), [randn(2) for i=1:3]),
-    (x->x[1]/log(x[2]^2+9.), [randn(2) for i=1:3]),
+const eps = 1e-8
+
+for (forig,dorig,points) in [
+    (x->x[1], Dict(1=>x->1.), [randn(1) for i=1:3]),
+    (x->x[1]+1., Dict(1=>x->1.), [randn(1) for i=1:3]),
+    (x->2x[1], Dict(1=>x->2.), [randn(1) for i=1:3]),
+    (x->-x[1], Dict(1=>x->-1.), [randn(1) for i=1:3]),
+    (x->x[1]^2, Dict(1=>x->2x[1]), [randn(1) for i=1:3]),
+    (x->x[1]^2.5, Dict(1=>x->2.5x[1]^1.5), [rand(1) for i=1:3]),
+    (x->1/x[1], Dict(1=>x->-x[1]^-2), [randn(1) for i=1:3]),
+    (x->sin(x[1]), Dict(1=>x->cos(x[1])), [randn(1) for i=1:3]),
+    (x->cos(x[1]), Dict(1=>x->-sin(x[1])), [randn(1) for i=1:3]),
+    (x->exp(x[1]), Dict(1=>x->exp(x[1])), [randn(1) for i=1:3]),
+    (x->x[1]+x[2], Dict(1=>x->1.,2=>x->1.), [randn(2) for i=1:3]),
+    (x->x[1]-x[2], Dict(1=>x->1.,2=>x->-1.),[randn(2) for i=1:3]),
+    (x->x[1]*x[2], Dict(1=>x->x[2],2=>x->x[1]),[randn(2) for i=1:3]),
+    (x->x[1]/x[2], Dict(1=>x->1/x[2],2=>x->-x[1]/x[2]^2),[randn(2) for i=1:3]),
+    (x->sin(x[1]/x[2]), Dict(1=>x->cos(x[1]/x[2])/x[2],2=>x->-cos(x[1]/x[2])*x[1]/x[2]^2), [randn(2) for i=1:3]),
+    (x->cos(x[1]-x[2])^2, Dict(1=>x->-2cos(x[1]-x[2])sin(x[1]-x[2]),2=>x->2cos(x[1]-x[2])sin(x[1]-x[2])), [randn(2) for i=1:3]),
+    (x->exp(2^x[1]+x[2]^3), Dict(1=>x->exp(2^x[1]+x[2]^3)*2^(x[1]-1)*log(2) ,2=>x->exp(2^x[1]+x[2]^3)*3*x[2]^2), [randn(2) for i=1:3]),
+    (x->x[1]/log(x[2]^2+9.), Dict(1=>x->1/log(x[2]^2+9.), 2=>x->-2x[2]x[1]/(x[2]^2+9.)/log(x[2]^2+9.)^2), [randn(2) for i=1:3]),
 ]
-    @test compare(f,func(f(Source())), points)
+    expr = forig(Source())
+    f = func(expr)
+    @test compare(forig,f, points)
+    
+    for (i,d) in deriv(expr)
+        @test compare(dorig[i],d,points)
+    end
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,10 @@
 using Test, SimpleNLModels
 import Random: seed!
-import LinearAlgebra: norm
 seed!(0)
 
 function compare(f1,f2,points)
     for point in points
-        norm(f1(point) .- f2(point)) <= 1e-8 || return false
+        abs(f1(point) - f2(point)) <= 1e-8 || return false
     end
     return true
 end

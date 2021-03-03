04e9373f417944e5f826e9ce4272262583dcf972
Sungho Shin <sungho.shin.ss@gmail.com>
2021-03-02 22:30:58 -0600

    nlp interface updated

--- example/benchmark_include.jl
+++ example/benchmark_include.jl
@@ -18,24 +18,15 @@ function nlm_ocp(;N=1,optimizer=SimpleNLModels.IpoptOptimizer,opt...)
     u=[variable(m,start = 0) for i=1:N,j=1:p]
     [constraint(m,x[1,i]-x0[i]) for i=1:n]
     for i=1:N
-        constraint(
-            m, -x[i+1,1] + x[i,1] + (x[i,2])*dt)
-        constraint(
-            m, -x[i+1,2] + x[i,2] + (u[i,1]*cos(x[i,7])*sin(x[i,8])*cos(x[i,9])+u[i,1]*sin(x[i,7])*sin(x[i,9]))*dt)
-        constraint(
-            m, -x[i+1,3] + x[i,3] + (x[i,4])*dt)
-        constraint(
-            m, -x[i+1,4] + x[i,4] + (u[i,1]*cos(x[i,7])*sin(x[i,8])*sin(x[i,9])-u[i,1]*sin(x[i,7])*cos(x[i,9]))*dt)
-        constraint(
-            m, -x[i+1,5] + x[i,5] + (x[i,6])*dt)
-        constraint(
-            m, -x[i+1,6] + x[i,6] + (u[i,1]*cos(x[i,7])*cos(x[i,8])-9.8)*dt)
-        constraint(
-            m, -x[i+1,7] + x[i,7] + (b*u[i,2]*cos(x[i,7])/cos(x[i,8])+u[i,3]*sin(x[i,7])/cos(x[i,8]))*dt)
-        constraint(
-            m, -x[i+1,8] + x[i,8] + (-b*u[i,2]*sin(x[i,7])+u[i,3]*cos(x[i,7]))*dt)
-        constraint(
-            m, -x[i+1,9] + x[i,9] + (b*u[i,2]*cos(x[i,7])*tan(x[i,8])+u[i,3]*sin(x[i,7])*tan(x[i,8])+u[i,4])*dt)
+        constraint(m, -x[i+1,1] + x[i,1] + (x[i,2])*dt)
+        constraint(m, -x[i+1,2] + x[i,2] + (u[i,1]*cos(x[i,7])*sin(x[i,8])*cos(x[i,9])+u[i,1]*sin(x[i,7])*sin(x[i,9]))*dt)
+        constraint(m, -x[i+1,3] + x[i,3] + (x[i,4])*dt)
+        constraint(m, -x[i+1,4] + x[i,4] + (u[i,1]*cos(x[i,7])*sin(x[i,8])*sin(x[i,9])-u[i,1]*sin(x[i,7])*cos(x[i,9]))*dt)
+        constraint(m, -x[i+1,5] + x[i,5] + (x[i,6])*dt)
+        constraint(m, -x[i+1,6] + x[i,6] + (u[i,1]*cos(x[i,7])*cos(x[i,8])-9.8)*dt)
+        constraint(m, -x[i+1,7] + x[i,7] + (b*u[i,2]*cos(x[i,7])/cos(x[i,8])+u[i,3]*sin(x[i,7])/cos(x[i,8]))*dt)
+        constraint(m, -x[i+1,8] + x[i,8] + (-b*u[i,2]*sin(x[i,7])+u[i,3]*cos(x[i,7]))*dt)
+        constraint(m, -x[i+1,9] + x[i,9] + (b*u[i,2]*cos(x[i,7])*tan(x[i,8])+u[i,3]*sin(x[i,7])*tan(x[i,8])+u[i,4])*dt)
     end
 
     objective(m,.5*sum(Q[j]*(x[i,j]-d(i,j))^2 for i=1:N for j=1:n) + .5*sum(R[j]*(u[i,j]^2) for i=1:N for j=1:p) + .5*sum(Qf[j]*(x[N+1,j]-d(N+1,j))^2 for j=1:n))
@@ -49,8 +40,6 @@ function jump_ocp(;N=1,optimizer=Ipopt.Optimizer)
     n = 9
     p = 4
     nd= 9
-
-
     x0 = [0,0,0,0,0,0,0,0,0]
     d = (i,j)->
         (j==1 ? 1*sin(2*pi/N*i) : 0) +
@@ -67,35 +56,16 @@ function jump_ocp(;N=1,optimizer=Ipopt.Optimizer)
     @variable(m,u[1:N,1:p],start = 0)
     @NLparameter(m,d[i=1:N+1,j=1:nd]== d(i,j))
     @constraint(m,[i=1:n],x[1,i]==x0[i])
-    @NLconstraint(
-        m,[i=1:N],x[i+1,1] == x[i,1] + (x[i,2])*dt)
-    @NLconstraint(
-        m,[i=1:N], x[i+1,2] == x[i,2] + 
-        (u[i,1]*cos(x[i,7])*sin(x[i,8])*cos(x[i,9])+u[i,1]*sin(x[i,7])*sin(x[i,9]))*dt)
-    @NLconstraint(
-        m,[i=1:N], x[i+1,3] == x[i,3] + 
-        (x[i,4])*dt)
-    @NLconstraint(
-        m,[i=1:N], x[i+1,4] == x[i,4] + 
-        (u[i,1]*cos(x[i,7])*sin(x[i,8])*sin(x[i,9])-u[i,1]*sin(x[i,7])*cos(x[i,9]))*dt)
-    @NLconstraint(
-        m,[i=1:N], x[i+1,5] == x[i,5] + 
-        (x[i,6])*dt)
-    @NLconstraint(
-        m,[i=1:N], x[i+1,6] == x[i,6] + 
-        (u[i,1]*cos(x[i,7])*cos(x[i,8])-9.8)*dt)
-    @NLconstraint(
-        m,[i=1:N], x[i+1,7] == x[i,7] + 
-        (b*u[i,2]*cos(x[i,7])/cos(x[i,8])+u[i,3]*sin(x[i,7])/cos(x[i,8]))*dt)
-    @NLconstraint(
-        m,[i=1:N], x[i+1,8] == x[i,8] + 
-        (-b*u[i,2]*sin(x[i,7])+u[i,3]*cos(x[i,7]))*dt)
-    @NLconstraint(
-        m,[i=1:N], x[i+1,9] == x[i,9] + 
-        (b*u[i,2]*cos(x[i,7])*tan(x[i,8])+u[i,3]*sin(x[i,7])*tan(x[i,8])+u[i,4])*dt)
-
-    @NLobjective(m,Min, .5*sum(Q[j]*(x[i,j]-d[i,j])^2 for i=1:N for j=1:n)
-                 + .5*sum(R[j]*(u[i,j]^2) for i=1:N for j=1:p)
+    @NLconstraint(m,[i=1:N],x[i+1,1] == x[i,1] + (x[i,2])*dt)
+    @NLconstraint(m,[i=1:N], x[i+1,2] == x[i,2] + (u[i,1]*cos(x[i,7])*sin(x[i,8])*cos(x[i,9])+u[i,1]*sin(x[i,7])*sin(x[i,9]))*dt)
+    @NLconstraint(m,[i=1:N], x[i+1,3] == x[i,3] + (x[i,4])*dt)
+    @NLconstraint(m,[i=1:N], x[i+1,4] == x[i,4] + (u[i,1]*cos(x[i,7])*sin(x[i,8])*sin(x[i,9])-u[i,1]*sin(x[i,7])*cos(x[i,9]))*dt)
+    @NLconstraint(m,[i=1:N], x[i+1,5] == x[i,5] + (x[i,6])*dt)
+    @NLconstraint(m,[i=1:N], x[i+1,6] == x[i,6] + (u[i,1]*cos(x[i,7])*cos(x[i,8])-9.8)*dt)
+    @NLconstraint(m,[i=1:N], x[i+1,7] == x[i,7] + (b*u[i,2]*cos(x[i,7])/cos(x[i,8])+u[i,3]*sin(x[i,7])/cos(x[i,8]))*dt)
+    @NLconstraint(m,[i=1:N], x[i+1,8] == x[i,8] + (-b*u[i,2]*sin(x[i,7])+u[i,3]*cos(x[i,7]))*dt)
+    @NLconstraint(m,[i=1:N], x[i+1,9] == x[i,9] + (b*u[i,2]*cos(x[i,7])*tan(x[i,8])+u[i,3]*sin(x[i,7])*tan(x[i,8])+u[i,4])*dt)
+    @NLobjective(m,Min, .5*sum(Q[j]*(x[i,j]-d[i,j])^2 for i=1:N for j=1:n) + .5*sum(R[j]*(u[i,j]^2) for i=1:N for j=1:p)
                  + .5*sum(Qf[j]*(x[N+1,j]-d[N+1,j])^2 for j=1:n))
     return m
 end

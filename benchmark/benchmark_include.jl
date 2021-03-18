6839893d7e3f7c542e832863773b24eedb858318
Sungho Shin <sungho.shin@wisc.edu>
2021-03-17 21:50:49 -0500

    benchmark clean up

--- benchmark/benchmark_include.jl
+++ benchmark/benchmark_include.jl
@@ -95,27 +95,6 @@ function nlm_luksan_vlcek_501(;N=1,optimizer=Ipopt.Optimizer,opt...)
     return m
 end
 
-function casadi_luksan_vlcek_501(;N=1,optimizer="ipopt",opt...)
-
-    opti = casadi.Opti();
-
-    x = [opti._variable() for i=1:N]
-    for i=1:N
-        opti.set_initial(x[i],mod(i,2)==1 ? -1.2 : 1.);
-    end
-    
-    opti.minimize( sum(100(x[i-1]^2-x[i])^2+(x[i-1]-1)^2 for i=2:N) )
-    for i=1:N-2
-        opti._subject_to(3x[i+1]^3+2*x[i+2]-5+sin(x[i+1]-x[i+2])sin(x[i+1]+x[i+2])+4x[i+1]-x[i]exp(x[i]-x[i+1])-3 == 0)
-    end
-    
-    opti.solver("ipopt");
-    sol = opti.solve();
-    
-    return  sol
-end
-
-
 function jump_luksan_vlcek_501(;N=1,optimizer=Ipopt.Optimizer)
     m=Model(optimizer)
     @variable(m,x[i=1:N], start= mod(i,2)==1 ? -1.2 : 1.)

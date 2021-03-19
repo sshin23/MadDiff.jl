c95a914a0c8322d99e486684776eb6d9fafa87ac
Sungho Shin <sungho.shin@wisc.edu>
2021-03-18 21:14:30 -0500

    common.jl improvement

--- src/Interfaces/common.jl
+++ src/Interfaces/common.jl
@@ -1,5 +1,8 @@
 sum_init_0(itr) = isempty(itr) ? 0 : sum(itr)
 
+const SNL = SimpleNLModels
+export SNL
+
 const VAR = Variable()
 const PAR = Parameter()
 struct HessianSource
@@ -43,7 +46,7 @@ end
 end
 
 function get_evaluators_obj(objs,p)
-    funcss = classify_obj_funcss(objs)
+    funcss = get_obj_funcss(objs)
     obj = [.0]
     @inline function (x)
         obj[] = 0.
@@ -55,7 +58,7 @@ function get_evaluators_obj(objs,p)
 end
 
 function get_evaluators_grad!(objs,p)
-    tupless = classify_tupless(get_grad_tuples(objs))
+    tupless = get_grad_tupless(objs)
     @inline function (f,x)
         f.=0
         @simd for l in eachindex(tupless)
@@ -65,7 +68,7 @@ function get_evaluators_grad!(objs,p)
 end
 
 function get_evaluators_con!(cons,p)
-    tupless = classify_tupless(get_con_tuples(cons))
+    tupless = get_con_tupless(cons)
     @inline function (c,x)
         c.= 0
         @simd for l in eachindex(tupless)
@@ -75,7 +78,7 @@ function get_evaluators_con!(cons,p)
 end
 
 function get_evaluators_jac!(cons,p)
-    tupless = classify_tupless(get_jac_tuples(cons))
+    tupless = get_jac_tupless(cons)
     
     @inline function jac!(J,x)
         @simd for l in eachindex(tupless)
@@ -93,7 +96,7 @@ function get_evaluators_jac!(cons,p)
 end
 
 function get_evaluators_hess!(grad!,jac!,p)
-    tupless = classify_tupless(get_hess_dict(get_obj_2nds(grad!.tupless),get_con_2nds(jac!.tupless)))
+    tupless = get_hess_tupless(get_hess_dict(get_obj_2nds(grad!.tupless),get_con_2nds(jac!.tupless)))
 
     @inline function hess!(H,x,lag,sig)
         @simd for l in eachindex(tupless)
@@ -112,15 +115,61 @@ function get_evaluators_hess!(grad!,jac!,p)
     return hess!,hess_sparsity!,nnz_hess
 end
 
-classify_tupless(dict) = (k=0; [[(i,k+=1,d) for (i,d) in dict if typeof(d)==type] for type in union(typeof(d) for (i,d) in dict)])
-classify_obj_funcss(objs) = [[func(objs[i]) for i in eachindex(objs) if typeof(func(objs[i]))==type] for type in union(typeof(func(obj)) for obj in objs)]
-
 get_obj_2nds(tupless) = [[(i,deriv(d(HessianSource(i),Parameter()))) for (i,j,d) in tuples] for tuples in tupless]
 get_con_2nds(tupless) = [[(k,i,deriv(d(HessianSource(i),Parameter()))) for ((k,i),j,d) in tuples] for tuples in tupless]
 
-get_grad_tuples(objs) = [(i,d) for obj in objs for (i,d) in deriv(obj)]
-get_con_tuples(cons) = [(i,func(cons[i])) for i in eachindex(cons)]
-get_jac_tuples(cons) = [((i,j),d) for i=1:length(cons) for (j,d) in deriv(cons[i])]
+push_dict!(d,dict) = haskey(dict,typeof(d)) ? push!(dict[typeof(d)],d) : (dict[typeof(d)] = [d])
+push_dict!(d,dict,i) = haskey(dict,typeof(d)) ? push!(dict[typeof(d)],(i,nothing,d)) : (dict[typeof(d)] = [(i,nothing,d)])
+push_dict!(d,dict,i,j,k) = haskey(dict,typeof(d)) ? push!(dict[typeof(d)],((i,j),k[]+=1,d)) : (dict[typeof(d)] = [((i,j),k[]+=1,d)])
+
+function get_obj_funcss(objs)
+    dict = Dict{DataType,Vector{F} where F <: Function}()
+    @simd for i in eachindex(objs)
+        push_dict!(func(objs[i]),dict)
+    end
+    return collect(values(dict))
+end
+function get_grad_tupless(objs)
+    dict = Dict{DataType,Vector{Tuple{Int,Nothing,F}} where F <: Function}()
+    @simd for i in eachindex(objs)
+        get_grad_tupless(objs[i],dict,i)
+    end
+    return collect(values(dict))
+end
+function get_grad_tupless(obj,dict,i)
+    for (j,d) in deriv(obj)
+        push_dict!(d,dict,j)
+    end
+end
+function get_con_tupless(cons)
+    dict = Dict{DataType,Vector{Tuple{Int,Nothing,F}} where F <: Function}()
+    @simd for i in eachindex(cons)
+        push_dict!(func(cons[i]),dict,i)
+    end
+    return collect(values(dict))
+end
+function get_jac_tupless(cons)
+    dict = Dict{DataType,Vector{Tuple{Tuple{Int,Int},Int,F}} where F <: Function}()
+    k = [0]
+    @simd for i in eachindex(cons)
+        get_jac_tupless(cons[i],dict,i,k)
+    end
+    return collect(values(dict))
+end
+function get_jac_tupless(con,dict,i,k)
+    for (j,d) in deriv(con)
+        push_dict!(d,dict,i,j,k)
+    end
+end
+function get_hess_tupless(hess)
+    dict = Dict{DataType,Vector{Tuple{Tuple{Int,Int},Int,F}} where F <: Function}()
+    k = [0]
+    for ((i,j),d) in hess
+        push_dict!(d,dict,i,j,k)
+    end
+    return collect(values(dict))
+end
+
 
 function get_hess_dict(obj_2nds,con_2nds)
     hess_dict = Dict{Tuple{Int,Int},Function}()
@@ -162,7 +211,6 @@ f_hess_obj_entry(d,dold) = @inline (x,p,sig,lag)->dold(x,p,sig,lag)+d(x,p)*sig
 f_hess_con_entry(d,k) = @inline (x,p,sig,lag)->d(x,p)*lag[k]
 f_hess_con_entry(d,dold,k) = @inline (x,p,sig,lag)->dold(x,p,sig,lag)+d(x,p)*lag[k]
 
-
 function get_nlp_functions(objs,cons,p)
     _obj = get_evaluators_obj(objs,p)
     _grad! = get_evaluators_grad!(objs,p)
@@ -170,7 +218,7 @@ function get_nlp_functions(objs,cons,p)
     _jac!,_jac_sparsity!,nnz_jac = get_evaluators_jac!(cons,p)
     _hess!,_hess_sparsity!,nnz_hess = get_evaluators_hess!(_grad!,_jac!,p)
     
-    (_obj,_grad!,_con!,_jac!,_jac_sparsity!,nnz_jac,_hess!,_hess_sparsity!,nnz_hess)
+    return _obj,_grad!,_con!,_jac!,_jac_sparsity!,nnz_jac,_hess!,_hess_sparsity!,nnz_hess
 end
 
 

6c689591701a53891ef0b2fa36c33b5888f01753
Sungho Shin <sshin@anl.gov>
2022-05-29 19:25:29 -0500

    ci update

--- .ci/ci.jl
+++ .ci/ci.jl
@@ -1,22 +1,3 @@
-using Pkg
-
-Pkg.update()
-deps = [
-    PackageSpec(name = "MathOptInterface", rev="master")
-    PackageSpec(name = "JuMP", rev="od/moi-nonlinear")
-]
-
-root_directory = dirname(@__DIR__)
-
-pkgs = [
-    PackageSpec(path = joinpath(root_directory, "lib", "MadDiffCore")),
-    PackageSpec(path = joinpath(root_directory, "lib", "MadDiffModels")),
-    PackageSpec(path = joinpath(root_directory, "lib", "MadDiffMOI")),
-    PackageSpec(path = joinpath(root_directory))
-]
-
-Pkg.add.(deps)
-Pkg.develop.(pkgs)
-Pkg.instantiate()
+include("build.jl")
 
 Pkg.test("MadDiff"; coverage = true)
